b3d25626b927bb06260d7dda20a2fad045af40c0
Robson Henrique Rodrigues <robhenrod@gmail.com>
2023-12-20 15:41:10 -0300

    Fundamentos de DDD - Value Object de Slug

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,21 +1,25 @@
 import { randomUUID } from "node:crypto"
+import { Slug } from "./value-objects/slug"
 
 interface QuestionProps {
   authorId: string
   title: string
+  slug: Slug
   content: string
 }
 
 export class Question {
   public id: string
-  public title: string
-  public content: string
   public authorId: string
+  public title: string
+  public slug: Slug
+  public content: string
 
   constructor(props: QuestionProps, id?: string,) {
     this.id = id ?? randomUUID()
     this.authorId = props.authorId
     this.title = props.title
+    this.slug = props.slug
     this.content = props.content
   }
 }
