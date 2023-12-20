33be8a772edabee4cafbbad69fc969824e72e70d
Robson Henrique Rodrigues <robhenrod@gmail.com>
2023-12-20 17:07:09 -0300

    Fundamentos de DDD - Classe base de entidades

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,4 +1,4 @@
-import { randomUUID } from "node:crypto"
+import { Entity } from "../../core/entities/entity"
 import { Slug } from "./value-objects/slug"
 
 interface QuestionProps {
@@ -8,18 +8,4 @@ interface QuestionProps {
   content: string
 }
 
-export class Question {
-  public id: string
-  public authorId: string
-  public title: string
-  public slug: Slug
-  public content: string
-
-  constructor(props: QuestionProps, id?: string,) {
-    this.id = id ?? randomUUID()
-    this.authorId = props.authorId
-    this.title = props.title
-    this.slug = props.slug
-    this.content = props.content
-  }
-}
+export class Question extends Entity<QuestionProps> {}
