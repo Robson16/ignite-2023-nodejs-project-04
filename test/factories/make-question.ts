59aa95be70b5257416295a301775826a16ef9219
Robson H. Rodrigues <robhenrod@gmail.com>
2024-01-25 16:14:11 -0300

    Fórum com DDD & Clean Architecture - Gerando dados fictícios

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,15 +1,23 @@
-import { UniqueEntityID } from "@/core/entities/unique-entity-id";
-import { Question, QuestionProps } from "@/domain/forum/enterprise/entities/question";
-import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import {
+  Question,
+  QuestionProps,
+} from '@/domain/forum/enterprise/entities/question'
+import { faker } from '@faker-js/faker'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
-  const question = Question.create({
-    authorId: new UniqueEntityID(),
-    title: 'Example question',
-    content: 'Question content',
-    ...override
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityID,
+) {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityID(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
-
