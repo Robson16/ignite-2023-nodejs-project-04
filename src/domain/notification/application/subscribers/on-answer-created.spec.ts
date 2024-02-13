27487a293a54d23552b0adf1bcb08368ae54d8f4
Robson H. Rodrigues <robhenrod@gmail.com>
2024-02-13 13:24:43 -0300

    Subdomínios & Domain Events - Ouvindo um evento de domínio

---
+++ src/domain/notification/application/subscribers/on-answer-created.spec.ts
@@ -0,0 +1,25 @@
+import { makeAnswer } from 'test/factories/make-answer'
+import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { OnAnswerCreated } from './on-answer-created'
+
+let inMemoryAnswersAttachmentsRepository: InMemoryAnswerAttachmentsRepository
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+
+describe('On Answer Created', () => {
+  beforeEach(() => {
+    inMemoryAnswersAttachmentsRepository =
+      new InMemoryAnswerAttachmentsRepository()
+    inMemoryAnswersRepository = new InMemoryAnswersRepository(
+      inMemoryAnswersAttachmentsRepository,
+    )
+  })
+
+  it('should send a notification when am answer is created', async () => {
+    const onAnswerCreated = new OnAnswerCreated()
+
+    const answer = makeAnswer()
+
+    await inMemoryAnswersRepository.create(answer)
+  })
+})
